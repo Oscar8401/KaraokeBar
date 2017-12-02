b9614ee8742cc6573feaa69f55a264fe06dcbf07
Taleb Mosyri <taleb_n1@hotmail.com>
2017-12-02 23:50:59 +0100

    Include the songs, txt files. Update all the metods. Create singer class

--- CaraokeMachine/CaraokeMachine/Program.cs
+++ CaraokeMachine/CaraokeMachine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CaraokeMachine
 {
@@ -9,7 +10,54 @@ namespace CaraokeMachine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //creating an array to hold the song that we want to be able to play
+            var songs = new string[] {"Sia", "Tove", "Walk" }; //the name of the song should be the name of the txt file
+            Console.WriteLine("Welcome to Singer!\n");
+            while (true)
+            { 
+            Console.WriteLine("Please choose the song you'd like to sing\n");
+  
+            Console.WriteLine("1) Sia ");
+            Console.WriteLine("2) ToveLo");
+            Console.WriteLine("3) Walk the moon");
+            Console.WriteLine("Type 'Quit' to quit the application");
+
+
+            var response = Console.ReadLine();
+                if (response.ToLower() == "quit")
+                {
+                    break;
+                }
+                //try to parse the first arrgment which is response and if it is successfull then will populate the second argument which is song 
+                var song = 0;
+                if (!int.TryParse(response, out song))
+                {
+                    Console.WriteLine("Invalid input try again");
+                    //it stop to execute any further code inside this loop and starts the loop over from the begining 
+                    continue;
+                }
+                //-- decrenets to one, because arrays count from 0
+                //so when they choose number one we want to reference the song at the index 0
+                song--;
+                var fileName = songs[song] + ".txt";
+                //to print out the filename(the song from the array)
+                Console.WriteLine(fileName);
+
+                if (!File.Exists(fileName))
+                {
+                    Console.WriteLine("File not found, try again");
+                    continue;
+                }
+                var lines = File.ReadAllLines(fileName);
+                Console.WriteLine("*************************************");
+
+                foreach (var line in lines)
+                {
+                    singer.WriteLine(line);
+                }
+
+                Console.WriteLine("*************************************");
+            }
         }
     }
 }
---
+++ CaraokeMachine/CaraokeMachine/singer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaraokeMachine
+{
+    public class singer
+    {
+        //by making the method static, that's mean we don't have t instatiate the singer
+        public static void WriteLine (string line)
+        {
+            var results = line.Split(':');
+            WriteLine(results[0],int.Parse(results[1]));
+        }
+        public static void WriteLine (string line, int time)
+        {
+            //how long it will take for each charackter to e load
+            var speed = line.Length;
+            foreach (var letter in line.ToCharArray())
+            {
+                Console.Write(letter);
+                //this method tell the application stop donig everything for some miliseconds
+                System.Threading.Thread.Sleep(speed);
+
+            }
+            Console.WriteLine("");
+            
+        }
+    }
+}
